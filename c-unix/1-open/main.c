7f290679734a146ec59cefc350dee21a5711cb31
alphaben <bennamrouchex@gmail.com>
2024-12-11 16:21:22 +0100

    add c-unix

---
+++ c-unix/1-open/main.c
@@ -0,0 +1,17 @@
+/****************************************************************************##
+##                            c unix course                                   ##
+##                            by alphaben                                     ##
+##                            2024/12                                         ##
+##                            https://www.youtube.com/@alphaben0              ##
+##*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+    
+    return 0;
+}
